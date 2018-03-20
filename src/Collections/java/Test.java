b4c8bbb14934852c6e642ff82a8971d1fd2e4f82
汤俞龙 <541976141@qq.com>
2018-03-21 00:37:23 +0800

    collection sort

--- src/Collections/java/Test.java
+++ src/Collections/java/Test.java
@@ -14,12 +14,37 @@ public class Test {
         System.out.println("find first java tag article = " + findFirstJava(articles));
         System.out.println("find first java tag article stream = " + findFirstJavaStream(articles));
 
-        Map<String, List<Article>> map =  groupByAuthor(articles);
+        Map<String, List<Article>> map = groupByAuthor(articles);
         for (String key : map.keySet()) {
             System.out.println("map.get(" + key + ") = " + map.get(key) + "\n-------------------");
         }
-        Map<String, List<Article>> StreamMap =  groupByAuthorStream(articles);
+        Map<String, List<Article>> StreamMap = groupByAuthorStream(articles);
         StreamMap.keySet().forEach(key -> System.out.println("StreamMap.get(" + key + ") = " + map.get(key) + "\n-------------------"));
+
+        List<RichArticle> richArticles = new ArrayList<>();
+        richArticles.addAll(initRichArticles());
+        sortedJava(richArticles);
+        sortedJava8_1(richArticles);
+        sortedJava8_2(richArticles);
+    }
+
+
+    private static List<RichArticle> initRichArticles() {
+        List<RichArticle> richArticles = new ArrayList<>();
+        List tags0 = Arrays.asList("集合", "函数式", "Java", "设计模式");
+        List tags1 = Arrays.asList("前端", "ios", "Swift");
+        List tags2 = Arrays.asList("翻译", "!PLAY");
+        List tags3 = Arrays.asList("Kafka", "消息队列", "分布式系统");
+        List tags4 = Arrays.asList("PHP", "x");
+
+        for (int i = 0; i < 2; i++) {
+            richArticles.add(new RichArticle("用 Swift 模仿 Vue + Vuex 进行 iOS 开发（二）：Coordinator" + i, "Yison", tags1, RandomDate.randomDate(), false));
+            richArticles.add(new RichArticle("<译> 响应式 Web 应用（五）" + i, "Shaw", tags2, RandomDate.randomDate(), false));
+            richArticles.add(new RichArticle("Kafka 学习笔记（一） ：为什么需要 Kafka？" + i, "Godpan", tags3, RandomDate.randomDate(), true));
+            richArticles.add(new RichArticle("如何24小时入门 Java " + i, "小王", tags0, RandomDate.randomDate(), false));
+            richArticles.add(new RichArticle("PHP 是最好的语言" + i, "小王", tags4, RandomDate.randomDate(), true));
+        }
+        return richArticles;
     }
 
     private static List<Article> initArticles() {
@@ -57,6 +82,7 @@ public class Test {
 
     /**
      * 按照作者对文章分组_循环处理
+     *
      * @param articles
      * @return
      */
@@ -76,11 +102,58 @@ public class Test {
 
     /**
      * 按照作者对文章分组_流处理
+     *
      * @param articles
      * @return
      */
     private static Map<String, List<Article>> groupByAuthorStream(List<Article> articles) {
         return articles.stream()
                 .collect(Collectors.groupingBy(Article::getAuthor));
+    }
+
+
+    /**
+     * 最原始Java多条件排序
+     *
+     * @param richArticles
+     */
+    private static void sortedJava(List<RichArticle> richArticles) {
+        Collections.sort(richArticles, new Comparator<RichArticle>() {
+            @Override
+            public int compare(RichArticle r1, RichArticle r2) {
+                int x = r1.getPinned().compareTo(r2.getPinned());
+                int y = r1.getCreateTime().compareTo(r2.getCreateTime());
+                if (x == 0) {
+                    return y;
+                }
+                return x;
+            }
+        });
+
+    }
+
+    /**
+     * lambda 版本排序
+     *
+     * @param richArticles
+     */
+    private static void sortedJava8_1(List<RichArticle> richArticles) {
+        richArticles.sort((r1, r2) -> {
+            int x = r1.getPinned().compareTo(r2.getPinned());
+            int y = r1.getCreateTime().compareTo(r2.getCreateTime());
+            if (x == 0) {
+                return y;
+            }
+            return x;
+        });
+    }
+
+    /**
+     * Java 8 排序_链式
+     *
+     * @param richArticles
+     */
+    private static void sortedJava8_2(List<RichArticle> richArticles) {
+        richArticles.sort(Comparator.comparing(RichArticle::getPinned).thenComparing(RichArticle::getCreateTime));
     }
 }
--- src/Collections/kotlin/Test.kt
+++ src/Collections/kotlin/Test.kt
@@ -1,6 +1,8 @@
 package Collections.kotlin
 
+import Collections.java.RandomDate.randomDate
 import java.util.*
+import kotlin.comparisons.compareBy
 
 /**
  * Created by prefert on 2018/3/18.
@@ -8,6 +10,7 @@ import java.util.*
 object Test {
     val articles = mutableListOf<Article>()
 
+    val richArticles = mutableListOf<RichArticle>()
     @JvmStatic
     fun main(args: Array<String>) {
         initArticles()
@@ -15,6 +18,10 @@ object Test {
 
         val map = groupByAuthorKotlin(articles)
         map.forEach { println("it.author=${it.key} it.article=${it.value} \n-------------------") }
+
+        initRichArticles()
+        richArticles.sortWith(compareBy({ it.isPinned }, { -1 * it.createTime.time }))
+        richArticles.forEach(::println)
     }
 
     private fun initArticles() {
@@ -30,6 +37,22 @@ object Test {
             articles.add(Article("Kafka 学习笔记（一） ：为什么需要 Kafka？ $i", "Godpan", tags3))
             articles.add(Article("如何24小时入门 Java $i", "小王", tags0))
             articles.add(Article("PHP 是最好的语言 $i", "小王", tags4))
+        }
+    }
+
+    private fun initRichArticles() {
+        val tags0 = listOf("集合", "函数式", "Java", "设计模式")
+        val tags1 = listOf("前端", "ios", "Swift")
+        val tags2 = listOf("翻译", "!PLAY")
+        val tags3 = listOf("Kafka", "消息队列", "分布式系统")
+        val tags4 = listOf("PHP", "x")
+
+        for (i in 0..2) {
+            richArticles.add(RichArticle("用 Swift 模仿 Vue + Vuex 进行 iOS 开发（二）：Coordinator $i", "Yison", tags1, randomDate(), false))
+            richArticles.add(RichArticle("<译> 响应式 Web 应用（五）$i", "Shaw", tags2, randomDate(), false))
+            richArticles.add(RichArticle("Kafka 学习笔记（一） ：为什么需要 Kafka？ $i", "Godpan", tags3, randomDate(), false))
+            richArticles.add(RichArticle("如何24小时入门 Java $i", "小王", tags0, randomDate(), true))
+            richArticles.add(RichArticle("PHP 是最好的语言 $i", "小王", tags4, randomDate(), true))
         }
     }
 
